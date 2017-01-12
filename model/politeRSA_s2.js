cd1a31153906c3fe38b6004a892f3c358f6e435b
ejyoon <ejyoon@stanford.edu>
2017-01-12 11:38:08 -0800

    new s2 model

--- model/politeRSA_s2.js
+++ model/politeRSA_s2.js
@@ -35,7 +35,8 @@ var alpha = 1.25
 var speakerOptimality = 10
 
 // measured in Experiment 1
-var literalSemanticsFromR = dataFromR.literalSemantics
+// var literalSemanticsFromR = dataFromR.literalSemantics
+var literalSemanticsFromR = [{"utterance":"not_amazing","1":1,"2":0.92,"3":0.8,"4":0.24,"5":0.04},{"utterance":"not_bad","1":0.01,"2":0.28,"3":0.84,"4":0.88,"5":0.84},{"utterance":"not_good","1":1,"2":0.88,"3":0.16,"4":0.01,"5":0.01},{"utterance":"not_okay","1":0.92,"2":0.76,"3":0.12,"4":0.01,"5":0.12},{"utterance":"not_terrible","1":0.04,"2":0.44,"3":0.96,"4":0.92,"5":0.92},{"utterance":"yes_amazing","1":0.01,"2":0.01,"3":0.04,"4":0.68,"5":1},{"utterance":"yes_bad","1":1,"2":0.96,"3":0.01,"4":0.01,"5":0.01},{"utterance":"yes_good","1":0.01,"2":0.04,"3":0.84,"4":1,"5":1},{"utterance":"yes_okay","1":0.01,"2":0.28,"3":0.96,"4":0.76,"5":0.6},{"utterance":"yes_terrible","1":0.96,"2":0.52,"3":0.08,"4":0.01,"5":0.04}]
 
 var literalSemanticsNoNull = _.object(map(function(lst){
   [lst.utterance, [lst[1], lst[2], lst[3], lst[4], lst[5]] ]
@@ -134,39 +135,7 @@ var speaker2 = function(state, intendedGoals) {
 //  "heart5": speaker2(5, {honesty:0.9, kindness: 0.9})
 // }
 
-//var expectations_nicehonest = {
-//  "heart1": speaker1(1, {honesty:0.9, kindness: 0.9}),
-//  "heart2": speaker1(2, {honesty:0.9, kindness: 0.9}),
-//  "heart3": speaker1(3, {honesty:0.9, kindness: 0.9}),
-//  "heart4": speaker1(4, {honesty:0.9, kindness: 0.9}),
-//  "heart5": speaker1(5, {honesty:0.9, kindness: 0.9})
-//}
-
-//var expectations_honest = {
-//  "heart1": speaker1(1, {honesty:0.9, kindness: 0.2}),
-//  "heart2": speaker1(2, {honesty:0.9, kindness: 0.2}),
-//  "heart3": speaker1(3, {honesty:0.9, kindness: 0.2}),
-//  "heart4": speaker1(4, {honesty:0.9, kindness: 0.2}),
-//  "heart5": speaker1(5, {honesty:0.9, kindness: 0.2})
-//}
-
-//var expectations_nice = {
-//  "heart1": speaker1(1, {honesty:0.2, kindness: 0.9}),
-//  "heart2": speaker1(2, {honesty:0.2, kindness: 0.9}),
-//  "heart3": speaker1(3, {honesty:0.2, kindness: 0.9}),
-//  "heart4": speaker1(4, {honesty:0.2, kindness: 0.9}),
-//  "heart5": speaker1(5, {honesty:0.2, kindness: 0.9})
-//}
-
-var expectations_s1 = {
-  "heart1": speaker1(1, {honesty:0.9, kindness: 0.9}),
-  "heart2": speaker1(2, {honesty:0.9, kindness: 0.9}),
-  "heart3": speaker1(3, {honesty:0.9, kindness: 0.9}),
-  "heart4": speaker1(4, {honesty:0.9, kindness: 0.9}),
-  "heart5": speaker1(5, {honesty:0.9, kindness: 0.9})
-}
-
-var expectations_s2 = {
+var expectations_nicehonest = {
   "heart1": speaker2(1, {honesty:0.9, kindness: 0.9}),
   "heart2": speaker2(2, {honesty:0.9, kindness: 0.9}),
   "heart3": speaker2(3, {honesty:0.9, kindness: 0.9}),
@@ -174,16 +143,48 @@ var expectations_s2 = {
   "heart5": speaker2(5, {honesty:0.9, kindness: 0.9})
 }
 
-var expectations = {
-  "s1": expectations_s1,
-  "s2": expectations_s2 
+var expectations_honest = {
+  "heart1": speaker2(1, {honesty:0.9, kindness: 0.1}),
+  "heart2": speaker2(2, {honesty:0.9, kindness: 0.1}),
+  "heart3": speaker2(3, {honesty:0.9, kindness: 0.1}),
+  "heart4": speaker2(4, {honesty:0.9, kindness: 0.1}),
+  "heart5": speaker2(5, {honesty:0.9, kindness: 0.1})
 }
 
-//var expectations = {
-//  "nicehonest": expectations_nicehonest,
-//  "honest": expectations_honest, 
-//  "nice": expectations_nice 
+var expectations_nice = {
+  "heart1": speaker2(1, {honesty:0.1, kindness: 0.9}),
+  "heart2": speaker2(2, {honesty:0.1, kindness: 0.9}),
+  "heart3": speaker2(3, {honesty:0.1, kindness: 0.9}),
+  "heart4": speaker2(4, {honesty:0.1, kindness: 0.9}),
+  "heart5": speaker2(5, {honesty:0.1, kindness: 0.9})
+}
+
+//var expectations_s1 = {
+//  "heart1": speaker1(1, {honesty:0.9, kindness: 0.9}),
+//  "heart2": speaker1(2, {honesty:0.9, kindness: 0.9}),
+//  "heart3": speaker1(3, {honesty:0.9, kindness: 0.9}),
+//  "heart4": speaker1(4, {honesty:0.9, kindness: 0.9}),
+//  "heart5": speaker1(5, {honesty:0.9, kindness: 0.9})
 //}
+
+//var expectations_s2 = {
+//  "heart1": speaker2(1, {honesty:0.9, kindness: 0.9}),
+//  "heart2": speaker2(2, {honesty:0.9, kindness: 0.9}),
+//  "heart3": speaker2(3, {honesty:0.9, kindness: 0.9}),
+//  "heart4": speaker2(4, {honesty:0.9, kindness: 0.9}),
+//  "heart5": speaker2(5, {honesty:0.9, kindness: 0.9})
+// }
+
+//var expectations = {
+//  "s1": expectations_s1,
+//  "s2": expectations_s2 
+//}
+
+var expectations = {
+  "nicehonest": expectations_nicehonest,
+  "honest": expectations_honest, 
+  "nice": expectations_nice 
+}
 
 expectations
 //print(literalSemanticsNoNull)
